c527aa8f2b7d228f6a7498f560e167f310ce43f2
delbug <81692059@qq.com>
2021-09-02 16:38:20 +0800

    change

--- webhook.js
+++ webhook.js
@@ -1,13 +1,38 @@
+const { sign } = require('crypto');
 let http = require('http');
-let server = http.createServer(function (req, res) {
+let crypto = require('crypto');
+function sign(body) {
+    return `sha1=` + crypto.creatHmac('sha1', SECRET).update(body).digest('hex');
+}
 
+
+let server = http.createServer(function (req, res) {
+    console.log('-----------------分割线-----------------');
     if (req.method === 'POST' && req.url === '/webhook') {
         console.log('req.method:', req.method, 'req.url:', req.url);
+        let buffers = [];
+        req.on('data', function (buffer) {
+            buffers.push(buffer);
+        })
+
+        req.on('end', function (buffer) {
+            let body = Buffer.concat(buffers);
+            let event = req.header['x-github-event'];
+            // github请求来的时候，要传递请求体body，另外会传一个singature过来，你需要验证签名对不对
+            let signature = req.headers['x-github-event'];
+            let signbody = sign(body);
+
+            console.log('signature:', signature);
+            console.log('signbody:', signbody);
+
+            if (signature !== signbody) {
+                return res.end('Not Allowed 签名不一样');
+            };
+        });
 
         res.setHeader('Content-Type', 'application/json');
         let str = JSON.stringify({ ok: true });
         console.log('str:', str);
-
         res.end(str);
     } else {
         res.end('Not Found');
