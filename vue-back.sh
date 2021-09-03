601312d2e6b3836a4a2e093433efd6d10ff824b3
delbug <81692059@qq.com>
2021-09-03 11:11:51 +0800

    change

--- vue-back.sh
+++ vue-back.sh
@@ -8,11 +8,11 @@ git clean -f
 echo '拉取最近代码'
 git pull origin main
 echo '开始执行构建'
-docker build -t vue-back:1.0 .
+docker build -t vue-back:2.0 .
 echo '停止旧容器并删除旧容器'
 docker stop vue-back-container
 docker rm vue-back-container
 echo "启动新容器"
-docker container run -p 3000:3000 --name vue-back-container -d vue-back
+docker container run -p 3000:3000 --name vue-back-container -d vue-back:2.0
 
 # 执行 sh vue-back.sh 
