b7d302ff0bce028e4795f6770456eee1e6c2eab6
delbug <81692059@qq.com>
2021-09-03 11:09:11 +0800

    change

--- vue-back.sh
+++ vue-back.sh
@@ -3,7 +3,7 @@ WORK_PATH="/root/user/projects/vue-back"
 
 cd $WORK_PATH
 echo "先清除老代码"
-git reset --head origin/main
+git reset --hard origin/main
 git clean -f
 echo '拉取最近代码'
 git pull origin main
